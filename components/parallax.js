b9f9fe273601c1f34c26bab33b5f0121c3bfae2d
Higor Cavalcanti Machado Botelho <hcmb@cin.ufpe.br>
2016-10-22 20:39:12 -0200

    Parallax images overlay

--- components/parallax.js
+++ components/parallax.js
@@ -5,11 +5,11 @@ var Parallax = React.createClass({
   render: function(){
     return(
       <div className="parallax-container">
-        <div className="parallax"><img src={this.props.pic}/></div>
+          <div className="parallax deep-orange"><img src={this.props.pic} className="overlay"/></div>
       </div>
     );
   }
 });
 
-ReactDOM.render(<Parallax pic="http://res.cloudinary.com/dkbuneg9h/image/upload/v1477099784/pet/team-1_ew62th.jpg"/>, document.getElementById('team-1'));
-ReactDOM.render(<Parallax pic="http://res.cloudinary.com/dkbuneg9h/image/upload/v1477099784/pet/team-2_dmasam.jpg"/>, document.getElementById('team-2'));
+ReactDOM.render(<Parallax pic="http://res.cloudinary.com/dkbuneg9h/image/upload/v1477172565/pet/team-2_yv03cq.jpg"/>, document.getElementById('team-1'));
+ReactDOM.render(<Parallax pic="http://res.cloudinary.com/dkbuneg9h/image/upload/v1477172859/pet/team-1_abc3cz.jpg"/>, document.getElementById('team-2'));
