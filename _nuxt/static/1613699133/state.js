339bb0d1ae1d4147b744d348bb4b38917567f4f9
Erick2280 <Erick2280@users.noreply.github.com>
2021-02-19 01:45:35 +0000

    Deploying to gh-pages from @ pet-informatica/pet-informatica.github.io@43459b9d96c5b333b657d36b52b19dc9e268d4f4 🚀

---
+++ _nuxt/static/1613699133/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwww\u002F_nuxt\u002Fstatic\u002F1613699133",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fwww\u002F",assetsPath:"\u002Fwww\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
