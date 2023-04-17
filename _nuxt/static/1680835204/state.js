84b2c24e99be824a2032231e075ac650720e25d9
AliceSales <75091620+AliceSales@users.noreply.github.com>
2023-04-17 13:41:12 +0000

    Deploying to gh-pages from @ pet-informatica/pet-informatica.github.io@28b76cc335b8d1851e0170a980c137708b2c6ef9 🚀

--- _nuxt/static/1680835204/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fwww\u002F_nuxt\u002Fstatic\u002F1680835204",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{app:{basePath:"\u002Fwww\u002F",assetsPath:"\u002Fwww\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b25b294c"}}}}(null));
